dd6ef7d363e9f1a753c74697f0faea30454752f3
bartek dec <decbartek@gmail.com>
2023-01-14 11:12:22 +0100

    added basic routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {Home, SingleCountry, Error} from './pages';
+import {Navbar} from './components';
+
 function App() {
     return (
-        <h1>app</h1>
+        <BrowserRouter>
+            <Navbar/>
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='country/:id' element={<SingleCountry/>}/>
+                <Route path='*' element={<Error/>}/>
+            </Routes>
+        </BrowserRouter>
     );
 }
 
---
+++ src/pages/SingleCountry.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SingleCountry = () => {
+    return (
+        <div>
+            <h1>single country</h1>
+        </div>
+    );
+};
+
+export default SingleCountry;
