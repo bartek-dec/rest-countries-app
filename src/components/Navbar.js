122d2a20b420bfc4e281eff53336d79260c164f7
bartek dec <decbartek@gmail.com>
2023-01-14 19:46:48 +0100

    added styles to the Navbar and change mode functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {BsFillMoonStarsFill, BsSunFill} from 'react-icons/bs';
 
 const Navbar = () => {
+    const [isLight, setIsLight] = useState(false);
+
+    const toggleMode = () => {
+        setIsLight(!isLight);
+        document.body.classList.toggle('light-mode')
+    }
+
     return (
-        <div>
-            <h1>navbar</h1>
-        </div>
+        <nav className='nav'>
+            <div className='nav-wrapper'>
+                <h2 className='nav-heading'>Where in the world?</h2>
+
+                <button type='button' className='btn' onClick={toggleMode}>
+                    {isLight ? <BsFillMoonStarsFill/> : <BsSunFill/>}
+                    <span className='btn-text'>{isLight ? 'dark mode' : 'light mode'}</span>
+                </button>
+            </div>
+        </nav>
     );
 };
 
