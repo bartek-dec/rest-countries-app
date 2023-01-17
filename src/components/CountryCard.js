09f55b55db1464953cf8b43acbb8a23528ef31f8
bartek dec <decbartek@gmail.com>
2023-01-17 20:20:22 +0100

    fixed small bug

--- src/components/CountryCard.js
+++ src/components/CountryCard.js
@@ -6,7 +6,7 @@ const CountryCard = ({flag, name, population, region, capital}) => {
             <img src={flag} className='card-img' alt='flag'/>
             <div className='card-content'>
                 <h3 className='card-heading'>{name}</h3>
-                <h4 className='card-info'>Population: <span>{population}</span></h4>
+                <h4 className='card-info'>Population: <span>{population.toLocaleString('en-US')}</span></h4>
                 <h4 className='card-info'>Region: <span>{region}</span></h4>
                 <h4 className='card-info'>Capital: <span>{capital}</span></h4>
             </div>
