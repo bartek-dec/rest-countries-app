0d83c559cfa9145f1ff7eba0b374feef9dda3cb1
bartek dec <decbartek@gmail.com>
2023-01-17 12:54:34 +0100

    added content to the error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import {Link} from "react-router-dom";
+import {BiArrowBack} from "react-icons/bi";
+import {ErrorComp} from "../components";
 
 const Error = () => {
     return (
-        <div>
-            <h1>error</h1>
-        </div>
+        <main className='main error'>
+            <Link to={'/'} className='btn-back'>
+                <span><BiArrowBack/></span>Back
+            </Link>
+            <ErrorComp message={`404. Not Found`}/>
+        </main>
     );
 };
 
